bff8d0be940a2a7185bd599e471ecc2810518a33
codekyz <codekyz0111@gmail.com>
2021-05-15 23:31:07 +0900

    Unit 17 -form component

--- src/App.vue
+++ src/App.vue
@@ -4,29 +4,8 @@
   <button @click="onToggle">Toggle</button> -->
   <div class="container">
     <h2>To-Do List</h2>
-    <form @submit.prevent="onSubmit">
-    <div class="d-flex">
-      <div class="flex-grow-1 mr-2">
-        <input
-          class="form-control"
-          type="text"
-          v-model="todo"
-          placeholder="Type new to-do"
-        >
-      </div>
-      <div>
-        <button 
-          class="btn btn-primary"
-          type="submit"
-        >
-          Add
-        </button>
-      </div>
-    </div>
-      <div v-show="hasError" style="color: red">
-        This field cannot be empty
-      </div>
-    </form>
+    <TodoSimpleform />
+    
     <div v-if="!todos.length">
       추가된 Todo가 없습니다
     </div>
@@ -67,8 +46,12 @@
 
 <script>
 import { ref } from 'vue';
+import TodoSimpleform from './components/TodoSimpleform.vue';
 
 export default {
+  components: {
+    TodoSimpleform
+  },
   setup() {
     // const toggle = ref(false);
     const todo = ref('');
