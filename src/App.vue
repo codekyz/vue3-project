d90b0c177841407b698b0c844546e5c009e3de1e
codekyz <codekyz0111@gmail.com>
2021-08-07 15:47:07 +0900

    Unit 24

--- src/App.vue
+++ src/App.vue
@@ -2,20 +2,31 @@
 
   <div class="container">
     <h2>To-Do List</h2>
+
+    <input
+      class="form-control"
+      type="text"
+      v-model="searchText"
+      placeholder="Search"
+    >
+    <hr/>
     <TodoSimpleform @add-todo="addTodo" />
     
-    <div v-if="!todos.length">
-      추가된 Todo가 없습니다
-    </div>
+    <div v-if="!filterTodos.length">
+      There is nothing to display
 
-    <TodoList :todos="todos" @toggleTodo="toggleTodo"/>
+    </div>
+    <TodoList :todos="filterTodos"
+    @toggle-Todo="toggleTodo"
+    @delete-todo="deleteTodo"
+    />
     <!-- 자식 컴포넌트에서 todos라는 이름으로 todos 데이터에 접근 가능 -->
 
   </div>
 </template>
 
 <script>
-import { ref } from 'vue';
+import { ref, computed } from 'vue';
 import TodoSimpleform from './components/TodoSimpleform.vue';
 import TodoList from './components/TodoList.vue'
 
@@ -36,15 +47,29 @@ export default {
       todos.value.push(todo);
     };
 
+    const deleteTodo = (index) => {
+      todos.value.splice(index, 1);
+    };
+
+
     const toggleTodo = (index) => {
       console.log(todos.value[index]);
       todos.value[index].completed = !todos.value[index].completed;
       console.log(todos.value[index]);
     };
 
-    const deleteTodo = (index) => {
-      todos.value.splice(index, 1);
-    };
+    const searchText = ref('');
+    const filterTodos = computed(() => {
+      if (searchText.value) {
+        return todos.value.filter(todo => {
+          return todo.subject.includes(searchText.value);
+        });
+      }
+
+      return todos.value;
+    });
+
+
 
     return {
       todos,
@@ -52,6 +77,8 @@ export default {
       tosoStyle,
       deleteTodo,
       toggleTodo,
+      searchText,
+      filterTodos,
     };
   }
 }
