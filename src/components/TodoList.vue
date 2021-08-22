ba39c53f1d9fea6c229bb019a6d1c86ae9f89f6c
codekyz <codekyz0111@gmail.com>
2021-08-22 20:15:15 +0900

    :art: Unit 67 Implement Modal to DeleteTodo
    teleport와 slot을 이용하여 Modal component 구현

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,83 +1,103 @@
 <template>
-  <div
-      v-for="(todo, index) in todos"
-      :key="todo.id"
-      class="card mt-2"
+  <div v-for="(todo, index) in todos" :key="todo.id" class="card mt-2">
+    <div
+      class="card-body p-2 d-flex align-items-center"
+      style="cursor: pointer"
+      @click="moveToPage(todo.id)"
     >
-      <div 
-        class="card-body p-2 d-flex align-items-center"
-        style="cursor: pointer"
-        @click="moveToPage(todo.id)"
-      >
-        <div class="flex-grow-1">
-          <input
-            class="ml-2 mr-2"
-            type="checkbox"
-            :checked="todo.completed"
-            @change="toggleTodo(index, $event)"
-            @click.stop
-          >
-          <span
-            :class="{ todo : todo.completed }"
-          >
-
-            {{ todo.subject }}
-          </span>
-        </div>
-        <div>
-          <button
-            class="btn btn-danger btn-sm"
-            @click.stop="deleteTodo(index)"
-          >
-            Delete
-          </button>
-        </div>
+      <div class="flex-grow-1">
+        <input
+          class="ml-2 mr-2"
+          type="checkbox"
+          :checked="todo.completed"
+          @change="toggleTodo(index, $event)"
+          @click.stop
+        />
+        <span :class="{ todo: todo.completed }">
+          {{ todo.subject }}
+        </span>
+      </div>
+      <div>
+        <button
+          class="btn btn-danger btn-sm"
+          @click.stop="openModal(todo.id)"
+        >
+          Delete
+        </button>
       </div>
     </div>
+  </div>
+  <teleport to="#modal">
+    <Modal 
+      v-if="showModal"
+      @close="closeModal"
+      @delete="deleteTodo"
+    />
+  </teleport>
 </template>
 
 <script>
-import { useRouter } from 'vue-router';
+import { useRouter } from "vue-router";
+import Modal from "@/components/DeleteModal.vue";
+import { ref } from 'vue';
 export default {
-    // props: ['todos']
-    // 오브젝트로 받아오면 type과 required 지정 가능
-    props: {
-        todos: {
-            type: Array,
-            required : true
-        }
+  components: {
+    Modal,
+  },
+  // props: ['todos']
+  // 오브젝트로 받아오면 type과 required 지정 가능
+  props: {
+    todos: {
+      type: Array,
+      required: true,
     },
-    emits: ['toggle-todo','delete-todo'],
-    setup(props, { emit }) {
-      const router = useRouter();
+  },
+  emits: ["toggle-todo", "delete-todo"],
+  setup(props, { emit }) {
+    const router = useRouter();
+    const showModal = ref(false);
+    const todoDeleteId = ref(null);
+    const toggleTodo = (index, event) => {
+      emit("toggle-todo", index, event.target.checked);
+    };
 
-        const toggleTodo = (index, event) => {
-            emit('toggle-todo', index, event.target.checked);
-        };
+    const openModal = (id) => {
+      todoDeleteId.value = id;
+      showModal.value = true;
+    };
 
-        const deleteTodo = (index) => {
-            emit('delete-todo', index);
-        };
+    const closeModal = () => {
+      todoDeleteId.value = null;
+      showModal.value = false;
+    };
 
-        const moveToPage = (todoId) => {
-          //router.push('/todos/'+todoId);
-          router.push({
-            name: 'Todo',
-            params: {
-              id: todoId
-            }
-          });
-        };
+    const deleteTodo = () => {
+      emit("delete-todo", todoDeleteId.value);
+      showModal.value = false;
+      todoDeleteId.value = null;
+    };
 
-        return {
-            toggleTodo,
-            deleteTodo,
-            moveToPage,
-        }
-    }
-}
+    const moveToPage = (todoId) => {
+      //router.push('/todos/'+todoId);
+      router.push({
+        name: "Todo",
+        params: {
+          id: todoId,
+        },
+      });
+    };
+
+    return {
+      toggleTodo,
+      deleteTodo,
+      moveToPage,
+      showModal,
+      openModal,
+      closeModal,
+    };
+  },
+};
 </script>
 
 <style>
-
 </style>
